dd725201d3a93d41b7bcd7fed4c76418f83b6505
isaac-martin <isaac@weareplayground.co>
2018-08-22 13:15:47 -0400

    added blank projects component to keep react happy

---
+++ src/templates/project.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const ProjectPostTemplate = () => {
+  return (
+    <section className="section">
+      <p>Empty until i decide to build these out</p>
+    </section>
+  )
+}
+
+export default ProjectPostTemplate
