1eb2a3863f2dc1dc74eb786fb24e126c4335849d
isaac-martin <isaac@weareplayground.co>
2018-08-22 12:10:22 -0400

    Refactor"

--- src/components/textarea.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TextArea = ({}) => (
-  <div>
-    <h2 />
-  </div>
-)
-
-export default TextArea
--- src/templates/build.js
+++ src/templates/build.js
@@ -3,24 +3,28 @@ import PropTypes from 'prop-types'
 import Content, { HTMLContent } from '../components/Content'
 import { graphql } from 'gatsby'
 
+import Layout from '../components/layout'
+
 export const BuildPageTemplate = ({ title, content, contentComponent }) => {
   const PageContent = contentComponent || Content
 
   return (
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                {title}
-              </h2>
-              <PageContent className="content" content={content} />
+    <Layout>
+      <section className="section section--gradient">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-10 is-offset-1">
+              <div className="section">
+                <h3 className="title is-size-3 has-text-weight-bold is-bold-light">
+                  {title}
+                </h3>
+                <PageContent className="content" content={content} />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </Layout>
   )
 }
 
