18eb8aa4d68c8754e4b8b9da10dcd558709d5fa1
isaac-martin <isaac@weareplayground.co>
2018-08-22 12:09:47 -0400

    Wrapped projects in layout

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,44 +3,48 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import { graphql } from 'gatsby'
 
+import Layout from '../components/layout'
+
 export default class ProjectsPage extends React.Component {
   render() {
     const { data } = this.props
     const { edges: posts } = data.allMarkdownRemark
 
     return (
-      <section className="section">
-        <div className="container">
-          <div className="content">
-            <h1 className="has-text-weight-bold is-size-2">
-              Selected Projects
-            </h1>
-          </div>
-          {posts.map(({ node: post }) => (
-            <div
-              className="content"
-              style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
-              key={post.id}
-            >
-              <p>
-                <Link className="has-text-primary" to={post.fields.slug}>
-                  {post.frontmatter.title}
-                </Link>
-                <span> &bull; </span>
-                <small>{post.frontmatter.date}</small>
-              </p>
-              <p>
-                {post.excerpt}
-                <br />
-                <br />
-                <Link className="button is-small" to={post.fields.slug}>
-                  Keep Reading →
-                </Link>
-              </p>
+      <Layout>
+        <section className="section">
+          <div className="container">
+            <div className="content">
+              <h1 className="has-text-weight-bold is-size-2">
+                Selected Projects
+              </h1>
             </div>
-          ))}
-        </div>
-      </section>
+            {posts.map(({ node: post }) => (
+              <div
+                className="content"
+                style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
+                key={post.id}
+              >
+                <p>
+                  <Link className="has-text-primary" to={post.fields.slug}>
+                    {post.frontmatter.title}
+                  </Link>
+                  <span> &bull; </span>
+                  <small>{post.frontmatter.date}</small>
+                </p>
+                <p>
+                  {post.excerpt}
+                  <br />
+                  <br />
+                  <Link className="button is-small" to={post.fields.slug}>
+                    Keep Reading →
+                  </Link>
+                </p>
+              </div>
+            ))}
+          </div>
+        </section>
+      </Layout>
     )
   }
 }
@@ -54,7 +58,7 @@ ProjectsPage.propTypes = {
 }
 
 export const pageQuery = graphql`
-  query IndexQuery {
+  query ProjectQuery {
     allMarkdownRemark(
       sort: { order: DESC, fields: [frontmatter___year] }
       filter: { frontmatter: { templateKey: { eq: "project" } } }
