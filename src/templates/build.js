130d81fdac318fbff0084198d59d9bd83f68eac1
isaac-martin <isaac@weareplayground.co>
2018-08-21 22:25:08 -0400

    Feeding Data Into Templates

---
+++ src/templates/build.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Content, { HTMLContent } from '../components/Content'
+import { graphql } from 'gatsby'
+
+export const BuildPageTemplate = ({ title, content, contentComponent }) => {
+  const PageContent = contentComponent || Content
+
+  return (
+    <section className="section section--gradient">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <div className="section">
+              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+                {title}
+              </h2>
+              <PageContent className="content" content={content} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+BuildPageTemplate.propTypes = {
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string,
+  contentComponent: PropTypes.func,
+}
+
+const BuildPage = ({ data }) => {
+  const { markdownRemark: post } = data
+
+  return (
+    <BuildPageTemplate
+      contentComponent={HTMLContent}
+      title={post.frontmatter.title}
+      content={post.html}
+    />
+  )
+}
+
+BuildPage.propTypes = {
+  data: PropTypes.object.isRequired,
+}
+
+export default BuildPage
+
+export const buildPageQuery = graphql`
+  query BuildPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`
